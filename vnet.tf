f166ac72245e501bf534fb1cc24fa06549ff52d2
root <root@p-8ynq7uyeyevw-workstation.c.instruqt-prod.internal>
2021-03-26 19:40:09 +0000

    Initial commit

--- vnet.tf
+++
@@ -1,6 +0,0 @@
-module "network" {
-  source  = "app.terraform.io/JamesSmith-Training/network/azurerm"
-  version = "3.0.1"
-  # insert required variables here
-  resource_group_name = "jamessmith-workshop"
-}
